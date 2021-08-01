953ca383b8f9d7357abb34d61a98962a5195e77f
Abdulmaleeq <malorbit360@gmail.com>
2021-08-01 09:12:51 +0100

    Arranged dynamic-programming folder

---
+++ dynamic-programming/bestSum.js
@@ -0,0 +1,32 @@
+// console.log("A");
+// Find the number of combinations to form 7
+// Using the fibonacci technique to solve the problem
+
+let arr = [23,2, 14];
+function bestSum(arr, targetSum, store = {}){
+    if(targetSum in store) return store[targetSum];
+    if(targetSum === 0) return [];
+    if(targetSum < 0) return null;
+
+    let shortestCombo =  null;
+
+    for(let x of arr){
+        const remainder = targetSum - x;
+        const hos = howSum(arr, remainder, store);
+
+        if( hos !== null){
+
+            let val = [...hos,x];
+            if(shortestCombo === null || val.length < shortestCombo.length ){
+                shortestCombo = val;
+            }
+        }
+    }
+
+    store[targetSum] =  shortestCombo;
+
+    return shortestCombo;
+
+}
+
+console.log(howSum([1,2,5,25], 100))
