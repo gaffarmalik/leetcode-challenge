74aeaf08760f81dc6a75933960ad339a56fac073
Abdulmaleeq <malorbit360@gmail.com>
2021-08-01 14:24:49 +0100

    Solved canConstruct on string

---
+++ dynamic-programming/canConstruct.js
@@ -0,0 +1,32 @@
+// console.log("A");
+// Find the number of combinations to form 7
+// Using the fibonacci technique to solve the problem
+
+function canConstruct(arr, targetString, memo = {}){
+    if(targetString in memo) return memo[targetString];
+    if(targetString === "") return true;
+
+    for(let x of arr){
+        if(targetString.indexOf(x) === 0 ){
+            //compute next step
+            const remainder = targetString.slice(x.length, targetString.length);
+            const answer = canConstruct(arr, remainder, memo);
+            
+            
+            if(answer === true) {
+                memo[targetString] = true;
+                return true;
+            }
+        }
+    }
+    memo[targetString] = false;
+
+    return false;
+
+}
+
+console.log(canConstruct(['ab','abc','cd','def','abcd'], "abcdef"))
+console.log(canConstruct(['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar'],"skateboard"))
+console.log(canConstruct(['a', 'p', 'ent', 'enter', 'ot', 'o', 't'],"enterapotentpot"))
+console.log(canConstruct(['e', 'ee', 'eee', 'eeee', 'eeeee', 'eeeeee'],
+"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeoooeeeeeeeeeeeeeef"))//slow sample
