28cb8930a0c121b1623fe698d51324735db34dd3
Abdulmaleeq <malorbit360@gmail.com>
2021-08-03 15:27:46 +0100

    Added answers to the questions I failed today

--- dynamic-programming/canConstruct.js
+++ dynamic-programming/canConstruct.js
@@ -2,24 +2,23 @@
 // Find the number of combinations to form 7
 // Using the fibonacci technique to solve the problem
 
-function canConstruct(arr, targetString, memo = {}){
+function canConstruct(arr, targetString){
     if(targetString in memo) return memo[targetString];
     if(targetString === "") return true;
 
+    const arr_result =[];
     for(let x of arr){
         if(targetString.indexOf(x) === 0 ){
             //compute next step
             const remainder = targetString.slice(x.length, targetString.length);
-            const answer = canConstruct(arr, remainder, memo);
+            const answer = canConstruct(arr, remainder);
             
             
             if(answer === true) {
-                memo[targetString] = true;
                 return true;
             }
         }
     }
-    memo[targetString] = false;
 
     return false;
 
