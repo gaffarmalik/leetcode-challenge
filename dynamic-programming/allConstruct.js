13003430b52a1af7d94588746f185459630e669d
Abdulmaleeq <malorbit360@gmail.com>
2021-08-11 14:13:34 +0100

    Added AllContruct memoization

--- dynamic-programming/allConstruct.js
+++ dynamic-programming/allConstruct.js
@@ -1,24 +1,23 @@
-// // Find the numbers that occur only once in an effective way
+// Find all the possible combinations of substring that can construct a word;
+let arr =['ab', 'abc', 'cd', 'def', 'abcd', 'ef', 'c']
 
-// function solution(arr){
+function allConstruct(target, wordBank){
+//Base Case
+if(target === "") return [[]];
+const result = [];
 
-// // console.log(arr)
-// let memo ={};
-// let result = []
-// for(let x=0; x < arr.length; x++){
-//     if(memo[arr[x]]) memo[arr[x]] = false;
-//     else memo[arr[x]] = true;
-// }
+for(let word of arr){
+    if(target.indexOf(word) === 0){
+        const suffix = target.slice(word.length);
+        const suffixWays = allConstruct(suffix, wordBank);
+        const targetWays = suffixWays.map(way => [ word, ...way])
+        result.push(...targetWays);
+        
+        }
+}
 
-// for(let y in memo){
-//     if(memo[y] === true ){
-//         result.push(y)
-//     }
-// }
+return result;
 
+}
 
-// return result;
-
-// }
-
-// console.log(solution([1,2,3,3,1,1,1,4,5,6,6,2,4]))
+console.log(allConstruct('abcdef', arr))
